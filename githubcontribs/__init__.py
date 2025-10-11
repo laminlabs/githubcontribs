3946b6a811f3b6222bdbda3e9e13670d976962cf
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-11 18:00:13 +0200

    🔖 Release 0.1.0

--- githubcontribs/__init__.py
+++ githubcontribs/__init__.py
@@ -1,5 +1,5 @@
 """Simple analytics for GitHub contributions across an organization."""
 
-__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.1.0"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from ._fetcher import Fetcher
