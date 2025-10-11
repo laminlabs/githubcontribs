f7b31737595b9e46d4e75e99384a864d4621374f
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-11 19:47:08 +0200

    ⬆️ Bump version

--- githubcontribs/__init__.py
+++ githubcontribs/__init__.py
@@ -1,6 +1,6 @@
 """Simple analytics for GitHub contributions across an organization."""
 
-__version__ = "0.1.0"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.2a1"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from ._fetcher import Fetcher
 from ._plotter import Plotter
